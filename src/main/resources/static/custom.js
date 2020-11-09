20db05563af96b8a5e95ecae4202a7e2b4e0c87b
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-09 09:08:09 -0600

    custom js

---
+++ src/main/resources/static/custom.js
@@ -0,0 +1,9 @@
+
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 35.227085, lng: -80.843124 },
+        zoom: 10
+    });
+}
