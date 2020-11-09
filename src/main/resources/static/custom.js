3b4a4a16c40869a3e17bbe4a78d5ec6db142c90d
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-09 11:02:59 -0600

    javascripts

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,9 +1,35 @@
 
 let map;
+let marker;
+// let coords = {lat: 35.227085, lng: -80.843124};
+let contentString = '<h2>' + city + ', ' + state + '</h2>';
+
+
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 35.227085, lng: -80.843124 },
-        zoom: 10
+
+    let infoWindow = new google.maps.InfoWindow({
+        content: contentString
     });
+
+    let image = {url: '/flower.png', scaledSize: new google.maps.Size(50,50)};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: coords,
+        zoom: 10,
+        scrollwheel: false
+    });
+
+    marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+        icon: image,
+        animation: google.maps.Animation.DROP,
+    });
+
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.open(map, marker);
+    });
+    
 }
